be6748068286d2dcd6326fb020d9a7bbbcad323b
aymmenkhs <aymenkhouas@gmail.com>
2021-02-07 21:01:28 +0100

    writing in csv is now a separate methode

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -42,7 +42,7 @@ public class Main {
     public static void main(String[] arg) throws IOException {
 
         List<Integer> numbers = new ArrayList(Arrays.asList(1000003, 2000003, 4000037, 8000009, 16000057, 32000011, 64000031, 128000003, 256000001, 512000009, 1024000009, 2048000011));
-
+        List<Results> results = new ArrayList<Results>();
 
 
         long startTime;
@@ -51,18 +51,6 @@ public class Main {
         long durationPart2;
         long durationPart3;
         boolean isPrime;
-
-        FileWriter csvWriter = new FileWriter("JAVA_TP2.csv");
-        csvWriter.append("nb");
-        csvWriter.append(",");
-        csvWriter.append("is_prime");
-        csvWriter.append(",");
-        csvWriter.append("T_part1");
-        csvWriter.append(",");
-        csvWriter.append("T_part2");
-        csvWriter.append(",");
-        csvWriter.append("T_part3");
-        csvWriter.append("\n");
 
         for (Integer num : numbers) {
 
@@ -82,19 +70,37 @@ public class Main {
             durationPart3 = (endTime - startTime);
 
             System.out.println("n = " + num.toString( ) + ", est premier : " + isPrime);
-
-            csvWriter.append(num.toString( ));
-            csvWriter.append(",");
-            csvWriter.append(Boolean.toString(isPrime));
-            csvWriter.append(",");
-            csvWriter.append(Long.toString(durationPart1));
-            csvWriter.append(",");
-            csvWriter.append(Long.toString(durationPart2));
-            csvWriter.append(",");
-            csvWriter.append(Long.toString(durationPart3));
-            csvWriter.append("\n");
+            results.add(new Results(num, isPrime, durationPart1, durationPart2, durationPart3));
         }
 
+        writeCSV(results);
+    }
+
+    public static void writeCSV(List<Results> results) throws IOException {
+        FileWriter csvWriter = new FileWriter("JAVA_TP2.csv");
+        csvWriter.append("nb");
+        csvWriter.append(",");
+        csvWriter.append("is_prime");
+        csvWriter.append(",");
+        csvWriter.append("T_part1");
+        csvWriter.append(",");
+        csvWriter.append("T_part2");
+        csvWriter.append(",");
+        csvWriter.append("T_part3");
+        csvWriter.append("\n");
+
+        for (Results result : results){
+            csvWriter.append(result.getN().toString( ));
+            csvWriter.append(",");
+            csvWriter.append(Boolean.toString(result.isPrime()));
+            csvWriter.append(",");
+            csvWriter.append(Long.toString(result.getDurationPart1()));
+            csvWriter.append(",");
+            csvWriter.append(Long.toString(result.getDurationPart2()));
+            csvWriter.append(",");
+            csvWriter.append(Long.toString(result.getDurationPart3()));
+            csvWriter.append("\n");
+        }
         csvWriter.flush( );
         csvWriter.close( );
     }
